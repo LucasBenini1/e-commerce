1a69f5b55bc9b381245ab97f58e8ad999e37434b
Lucas Benini <lucas.andrade@ADM.UNIP.BR>
2026-08-17 18:34:15 -0300

    fix(cart): Quantidade de itens no carrinho

--- src/app/services/cart-service.ts
+++ src/app/services/cart-service.ts
@@ -11,26 +11,53 @@ export class CartService {
   public statusCart = signal(false);
 
   public uniqueItems = computed(() => {
-    return [...new Set(this.selectedItems().map(item => item.name))];
+    const items = this.selectedItems();
+
+    const groupedItems = new Map<number, { item: Item; quantity: number }>();
+
+    items.forEach(item => {
+      const existingItem = groupedItems.get(item.id);
+
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        groupedItems.set(item.id, {
+          item,
+          quantity: 1,
+        });
+      }
+    });
+
+    return Array.from(groupedItems.values());
   });
 
   private loadItems(): Item[] {
     const savedItems = localStorage.getItem(this.storageKey);
-    return savedItems ? (JSON.parse(savedItems) as Item[]) : [];
+
+    return savedItems ? (JSON.parse(savedItems) as Item[]): [];
   }
 
   addItem(item: Item): void {
     this.selectedItems.update(items => {
       const updatedItems = [...items, item];
+
       localStorage.setItem(this.storageKey, JSON.stringify(updatedItems));
+
       return updatedItems;
     });
   }
 
   removeItem(item: Item): void {
     this.selectedItems.update(items => {
-      const updatedItems = items.filter(i => i.id !== item.id);
+      const index = items.findIndex(i => i.id === item.id);
+
+      if (index === -1) return items;
+
+      const updatedItems = [...items];
+      updatedItems.splice(index, 1);
+
       localStorage.setItem(this.storageKey, JSON.stringify(updatedItems));
+
       return updatedItems;
     });
   }
