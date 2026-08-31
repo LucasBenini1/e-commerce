aef693733e080fd7ca4eb44e205be977eed50e6c
Lucas Benini <lucas.andrade@ADM.UNIP.BR>
2026-08-31 11:13:54 -0300

    feat(navbar): Adição dos ícones

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,15 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart-service';
 import { RouterLink } from '@angular/router';
+import { LucideAngularModule, House, Info, Mail, ShoppingCart } from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
   public cartService = inject(CartService);
+
+  readonly House = House;
+  readonly Info = Info;
+  readonly Mail = Mail;
+  readonly ShoppingCart = ShoppingCart;
 
   openCart(){
     this.cartService.toggleCart();
